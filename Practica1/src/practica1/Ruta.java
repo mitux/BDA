2da80aa31f330eac8c3f536c400217676f104199
mat.aules <mat.aules@Id41346.ub.edu>
2016-09-20 20:57:20 +0200

    practica 1 first commit

---
+++ Practica1/src/practica1/Ruta.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1;
+
+/**
+ *
+ * @author mat.aules
+ */
+public class Ruta {
+    
+    public Ruta(){
+        
+    }
+    
+}
